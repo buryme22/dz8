80702a1e88073a908b74cbdc2ff9086b31f20c89
Alexey <redy22@mail.ru>
2022-12-18 21:00:07 +0400

    new1

--- Program.cs
+++ Program.cs
@@ -8,37 +8,95 @@
 // 9 5 3 2
 // 8 4 4 2
 
-Console.WriteLine("Введите количество строк: ");
-int linesVol = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество строк: ");
+// int linesVol = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов: ");
+// int columnsVol = Convert.ToInt32(Console.ReadLine());
+// int[,] numbers = new int[linesVol, columnsVol];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine();
+// Console.WriteLine("Массив до изменения: ");
+// PrintArray(numbers);
+// for (int i = 0; i < numbers.GetLength(0); i++)
+// {
+//     for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+//     {
+//         for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+//         {
+//             if (numbers[i, z] < numbers[i, z + 1])
+//             {
+//                 int temp = 0;
+//                 temp = numbers[i, z];
+//                 numbers[i, z] = numbers[i, z + 1];
+//                 numbers[i, z + 1] = temp;
+//             }
+//         }
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine("Массив с упорядоченными значениями: ");
+// PrintArray(numbers);
+// void FillArrayRandomNumbers(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(0, 10);
+//         }
+//     }
+// }
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine("");
+//     }
+// }
 
-Console.WriteLine("Введите количество столбцов: ");
-int columnsVol = Convert.ToInt32(Console.ReadLine());
 
-int[,] numbers = new int[linesVol, columnsVol];
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+Console.WriteLine("Введите размер квадратного массива: ");
+int massVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[massVol, massVol];
 FillArrayRandomNumbers(numbers);
-Console.WriteLine();
-Console.WriteLine("Массив до изменения: ");
 PrintArray(numbers);
+int minsum = Int32.MaxValue;
+int indexLine = 0;
 
 for (int i = 0; i < numbers.GetLength(0); i++)
 {
-    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+    int sum = 0;
+    for (int j = 0; j < numbers.GetLength(1); j++)
     {
-        for (int z = 0; z < numbers.GetLength(1) - 1; z++)
-        {
-            if (numbers[i, z] < numbers[i, z + 1])
-            {
-                int temp = 0;
-                temp = numbers[i, z];
-                numbers[i, z] = numbers[i, z + 1];
-                numbers[i, z + 1] = temp;
-            }
-        }
+        sum = sum + numbers[i, j];        
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        indexLine++;
     }
 }
-Console.WriteLine();
-Console.WriteLine("Массив с упорядоченными значениями: ");
-PrintArray(numbers);
+
+Console.WriteLine("Строка с наименьшей суммой елементов под номером: " + (indexLine) + ", с суммой елементов равной: " + (minsum));
 
 void FillArrayRandomNumbers(int[,] array)
 {
